049485dcf1ec31e10d69b47b10329dd3e7b0fed0
Aleksa <lukicaleksa04@gmail.com>
2020-04-21 19:55:27 +0200

    Postavljena pozadina na stranici ograde

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { BetonskeOgradeComponent } from './betonske-ograde/betonske-ograde.component';
 
 
 const routes: Routes = [
-  {path:'home',component:HomeComponent}
+  {path:'home',component:HomeComponent},
+  {path:'ograde',component:BetonskeOgradeComponent}
 ];
 
 @NgModule({
