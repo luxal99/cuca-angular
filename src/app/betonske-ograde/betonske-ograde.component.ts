6f09a99a7b3083c133bc9cd514f52a38c3349960
Aleksa <lukicaleksa04@gmail.com>
2020-04-22 11:26:09 +0200

    Dodate kartice ograda

--- src/app/betonske-ograde/betonske-ograde.component.ts
+++ src/app/betonske-ograde/betonske-ograde.component.ts
@@ -12,4 +12,23 @@ export class BetonskeOgradeComponent implements OnInit {
   ngOnInit() {
   }
 
+  leftArray =
+    [
+      "Visoka otpornost i dugotrajnost",
+      "Nije potrebno odrzavanje",
+      "Otpornost na sve vremenske uslove",
+      "Sigurnost",
+      "Privatnost",
+      " Povoljna cena"
+    ]
+
+  rightArray =
+    [
+      "Brzina montaze",
+      "Zahvaljujuci brzoj gradnji izrada privremenih ograda postaje nepotrebna",
+      "Zauzimaju minimalan prostor",
+      "U rasponu od 0.4 - 2.5m",
+      "Montaza je moguca kako na ravnim povrsini tako i na kosini"
+    ]
+
 }
